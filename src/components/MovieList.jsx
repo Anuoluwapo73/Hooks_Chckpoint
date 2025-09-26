3a578f173509d797c4d2ed6a33fcaa2d59d331d8
Anuoluwapo Shoyode <shoyodeksodiq@gmail.com>
2025-09-26 18:45:31 +0100

    A

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import MovieCard from "./MovieCard";
+import { width } from "@fortawesome/free-solid-svg-icons/fa0";
 
 //Movie component
-const MovieList = ({movies}) => {
+const MovieList = ({ movies }) => {
   return (
     <>
-      <div className="movie-list" >
+      <div className="movie-list">
+        <br/><h1 style={{width: "100%", paddingLeft:"13px"}}>Movies</h1>
         {movies.map((movieItem, index) => (
           <MovieCard key={index} movie={movieItem} />
         ))}
