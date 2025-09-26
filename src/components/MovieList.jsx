0c2666f68c9d79fba1df16927aa8573915d7c45c
Anuoluwapo Shoyode <shoyodeksodiq@gmail.com>
2025-09-26 18:54:38 +0100

    A

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,8 +6,9 @@ import { width } from "@fortawesome/free-solid-svg-icons/fa0";
 const MovieList = ({ movies }) => {
   return (
     <>
+      <br />
+      <h1 style={{ width: "100%", paddingLeft: "13px", paddingTop:"80px"}}>Movies</h1>
       <div className="movie-list">
-        <br/><h1 style={{width: "100%", paddingLeft:"13px"}}>Movies</h1>
         {movies.map((movieItem, index) => (
           <MovieCard key={index} movie={movieItem} />
         ))}
